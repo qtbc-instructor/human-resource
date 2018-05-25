fe9d50333a2d07b0f7def9416d84a874c0ff65c0
川元浩明 <h-kawamoto@froide.co.jp>
2018-05-25 17:14:16 +0900

    tuika

--- company_mainpage.php
+++ company_mainpage.php
@@ -4,20 +4,20 @@
   <meta charset="utf-8">
 </head>
 <body>
-<h1 class="company_approval_pending">承認待ち</h1>
+<p class="company_approval_pending">承認待ち</p>
 <?php
   echo "承認待ちの講師が表示されます";
  ?>
-<h1 class="company_notification">通知</h1>
+<p class="company_notification">通知</p>
 <?php
   echo "承認されたか拒否されたか表示されます";
  ?>
-<h1 class"company_valution">評価登録</h1>
+<p class"company_valution">評価登録</p>
 <?php
   echo "承認状態の講師を評価します";
  ?>
 <button id="company_valution_button"><a href="company_mainpage.php">評価登録評価する</a></button>
-<h1 class="company_search_item" >検索</h1>
+<p class="company_search_item" >検索</p>
 <?php
   echo "検索項目の選択と検索が実行できます";
 ?>
