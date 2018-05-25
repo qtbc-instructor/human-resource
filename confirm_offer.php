d7b9105c72837a513fffce2812aac7829e68daa4
frdm08 <frdm08@frdm08.local>
2018-05-25 17:05:11 +0900

    とりあえずのページ

---
+++ confirm_offer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE>
+<html>
+<head>
+  <mata charset="utf-8">
+    <title>確認</title>
+  </head>
+  <body>
+    <div>
+      <?php
+
+
+       ?>
+
+       <form method="POST" action="complete_company.php">
+         <ul>
+           <input type="button" value="戻る" onclick="location.href='search_result.php'">
+           <input type="submit" value="確認">
+         </ul>
+       </form>
+       <div>
+       </body>
+     </html>
---
+++ search_result.php
@@ -0,0 +1,22 @@
+<!DOCTYPE>
+<html>
+<head>
+  <mata charset="utf-8">
+    <title>検索結果</title>
+  </head>
+  <body>
+    <div>
+      <?php
+
+
+       ?>
+
+       <form method="POST" action="confirm_offer.php">
+         <ul>
+           <input type="button" value="戻る" onclick="location.href='company_mainpage.php'">
+           <input type="submit" value="確認">
+         </ul>
+       </form>
+       <div>
+       </body>
+     </html>
