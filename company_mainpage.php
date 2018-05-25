cff07f6e79b53cd366ca0d1a46fa1a9b19a70be2
川元浩明 <h-kawamoto@froide.co.jp>
2018-05-25 16:52:10 +0900

    htmlのみ

---
+++ company_mainpage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+<h1 class="company_approval_pending">承認待ち</h1>
+<?php
+  echo "承認待ちの講師が表示されます";
+ ?>
+<h1 class="company_notification">通知</h1>
+<?php
+  echo "承認されたか拒否されたか表示されます";
+ ?>
+<h1 class"company_valution">評価登録</h1>
+<?php
+  echo "承認状態の講師を評価します";
+ ?>
+<button id="company_valution_button"><a href="company_mainpage.php">評価登録評価する</a></button>
+<h1 class="company_search_item" >検索</h1>
+<?php
+  echo "検索項目の選択と検索が実行できます";
+?>
+<button class="company_serach_item_button"><a href="search_result.php">検索</a></button>
+</body>
+</html>
