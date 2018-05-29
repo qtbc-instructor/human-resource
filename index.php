7041e9d9a3155d71a9360593f768fdebea47121c
frdm09 <frdm09@frdm09.local>
2018-05-29 15:56:25 +0900

    mainpageでkillsession()を追加

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+session_start();
+
+killSession();
+// セッションを破棄する
+function killSession(){
+  // セッション変数の値を空にする
+  $_SESSION = [];
+  // セッションクッキーを破棄する
+  if (isset($_COOKIE[session_name()])){
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time()-36000, $params['path']);
+  }
+  // セッションを破棄する
+  session_destroy();
+}
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -6,26 +25,26 @@
 </head>
 
 <body>
-  <?php 
+  <?php
   require_once("./FormCheck.php");
-  
+
   echo $_POST["login_address"];
-  
+
   if(!isset($_POST["login_address"])){
     echo "未入力項目があります。";
   }
-    
+
   // if($value == "sex" && !in_array($_POST[$value],$check_sex)){
   //   $err[] = $value."　の送られたデータが不正です。";
-  // } 
-  
-  
-  
-  
+  // }
+
+
+
+
    ?>
-  
-  
-  
+
+
+
   <!-- 登録済みアカウントログインフォーム -->
   <div>
     <form method="post" id="loginForm">
@@ -40,7 +59,7 @@
       </ul>
     </form>
   </div>
-  
+
   <!-- 新規アカウントログインフォーム -->
   <div>
     <label>新規登録はコチラ：
@@ -48,15 +67,15 @@
       <a href="entry_lecture.php">講師用</a>
     </label>
   </div>
-  
+
   <script type="text/javascript">
     var email = document.getElementById("email");
     var password = document.getElementById("password");
     var loginButton = document.getElementById("loginButton");
-      
+
     email.onblur = function(){ chkRegEmail(email.value);};
     password.onblur = function(){ isHanAlpha(password);};
-    
+
     function chkRegEmail(str){
       var Seiki=/[!#-9A-~]+@+[a-z0-9]+.+[^.]$/i;
       if(str!=""){
@@ -69,28 +88,28 @@
         alert("メールアドレスを入力してください");
       }
     };
-   
+
     function isHanAlpha(obj){
-      var str=obj.value; 
+      var str=obj.value;
       if(str!=""){
         if(str.match(/^[A-Za-z0-9]*$/)){
           if(str.length > 6 && str.length < 12){
             alert("6文字以上12文字以内で入力して下さい");
           }
         } else {
-          alert("半角英数字で入力して下さい。");  
+          alert("半角英数字で入力して下さい。");
         }
       } else {
         alert("パスワードを入力して下さい")
       }
     };
-    
+
     // 企業・講師選択ボタン
     loginButton.addEventListener('click', function() {
       var loginForm = document.getElementById( "loginForm" ) ;
       var loginForm_selected = loginForm.select_page ;
       var selected_value = loginForm_selected.value ;
-	  
+
       if(selected_value === "company"){
         alert("company");
         loginForm.setAttribute('action','sample.php');
@@ -100,9 +119,9 @@
         loginForm.setAttribute('action','sample.php');
         loginForm.submit();
       } else {
-        alert("企業・講師を選択して下さい。");  
+        alert("企業・講師を選択して下さい。");
       }
-    });  
+    });
   </script>
 </body>
 </html>
