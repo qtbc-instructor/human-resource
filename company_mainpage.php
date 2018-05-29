192bb12028b674d4c27aa70d2df44ffaaafaffd3
川元浩明 <h-kawamoto@froide.co.jp>
2018-05-29 17:16:22 +0900

    機能実装完了

--- company_mainpage.php
+++ company_mainpage.php
@@ -2,130 +2,201 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 </head>
 <body>
-<p class="company_approval_pending">承認待ち</p>
-<?php
-$user = "root";
-$pass = "mariadb";
-$dbh =new PDO('mysql:host=localhost;dbname=lcmatching_db;charset=utf8', $user, $pass);
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = "SELECT id,name, begin, status FROM lecture,status WHERE lecture.id = lecture_id AND status = 0";
-$stmt = $dbh->query($sql);
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$dbh = null;
+  <p class="company_approval_pending">承認待ち</p>
+  <?php
+  $user = "root";
+  $pass = "mariadb";
+  $dbh =new PDO('mysql:host=localhost;dbname=lcmatching_db;charset=utf8', $user, $pass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "SELECT id,name, begin, status FROM lecture,status WHERE lecture.id = lecture_id AND status = 0";
+  $stmt = $dbh->query($sql);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $dbh = null;
+  echo "<div class='syounin'>";
+  echo "<table border='1' class='syounin'>";
+  echo "<thead><tr>";
+  echo "<th>","講師名","</th>";
+  echo "<th>","日付","</th>";
+  echo "<th>","状態","</th>";
+  echo "</thead></tr>";
+  echo "<tbody>";
+  foreach($result as $approvalPending) {
+    echo "<tr>";
+    echo "<td>", $approvalPending['name'], "</td>";
+    echo "<td>", $approvalPending['begin'], "</td>";
+    echo "<td>", $approvalPending['status'], "</td>";
+    echo "</tr>";
+  }
+  echo "<tbody>";
+  echo "</table>";
+  echo "</div>";
+  ?>
 
-echo "<table border='1'>";
-echo "<thead><tr>";
-echo "<th>","講師名","</th>";
-echo "<th>","日付","</th>";
-echo "</thead></tr>";
-echo "<tbody>";
-foreach($result as $approvalPending) {
-  echo "<tr>";
-  echo "<td>", $approvalPending['name'], "</td>";
-  echo "<td>", $approvalPending['begin'], "</td>";
-  echo "</tr>";
-}
-echo "<tbody>";
-echo "</table>";
-?>
+  <p class="company_notification">承認済み</p>
+  <?php
+  $user = "root";
+  $pass = "mariadb";
+  $dbh =new PDO('mysql:host=localhost;dbname=lcmatching_db;charset=utf8', $user, $pass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "SELECT id,name, begin, status FROM lecture,status WHERE lecture.id = lecture_id AND status = 1";
+  $sqlNg = "SELECT id,name, begin, status FROM lecture,status WHERE lecture.id = lecture_id AND status = 4";
+  $stmt = $dbh->query($sql);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $dbh = null;
 
-<p class="company_notification">通知</p>
-<?php
-$user = "root";
-$pass = "mariadb";
-$dbh =new PDO('mysql:host=localhost;dbname=lcmatching_db;charset=utf8', $user, $pass);
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = "SELECT id,name, begin, status FROM lecture,status WHERE lecture.id = lecture_id AND status = 1";
-$stmt = $dbh->query($sql);
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$dbh = null;
+  echo "<table border='1' class='syouninzumi'>";
+  echo "<thead><tr>";
+  echo "<th>","講師名","</th>";
+  echo "<th>","日付","</th>";
+  echo "<th>","状態","</th>";
+  echo "</thead></tr>";
+  echo "<tbody>";
+  foreach($result as $approvalPending) {
+    echo "<tr>";
+    echo "<td>", $approvalPending['name'], "</td>";
+    echo "<td>", $approvalPending['begin'], "</td>";
+    echo "<td>", $approvalPending['status'], "</td>";
+    echo "</tr>";
+  }
+  echo "<tbody>";
+  echo "</table>";
+  echo"<br>";
+  ?>
 
-echo "<table border='1'>";
-echo "<thead><tr>";
-echo "<th>","講師名","</th>";
-echo "<th>","日付","</th>";
-echo "</thead></tr>";
-echo "<tbody>";
-foreach($result as $approvalPending) {
-  echo "<tr>";
-  echo "<td>", $approvalPending['name'], "</td>";
-  echo "<td>", $approvalPending['begin'], "</td>";
-  echo "</tr>";
-}
-echo "<tbody>";
-echo "</table>";
-?>
+  <p class="company_notification">拒否されました</p>
+  <?php
+  $user = "root";
+  $pass = "mariadb";
+  $dbh =new PDO('mysql:host=localhost;dbname=lcmatching_db;charset=utf8', $user, $pass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "SELECT id,name, begin, status FROM lecture,status WHERE lecture.id = lecture_id AND status = 4";
+  $stmt = $dbh->query($sql);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $dbh = null;
 
-<!-- 未実装状態 -->
-<p class"company_valution">評価登録</p>
-<?php
-$user = "root";
-$pass = "mariadb";
-$dbh =new PDO('mysql:host=localhost;dbname=lcmatching_db;charset=utf8', $user, $pass);
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = "SELECT id,name, begin, status FROM lecture,status WHERE lecture.id = lecture_id AND status = 1";
-$stmt = $dbh->query($sql);
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$dbh = null;
+  echo "<table border='1' class='kyohi'>";
+  echo "<thead><tr>";
+  echo "<th>","講師名","</th>";
+  echo "<th>","日付","</th>";
+  echo "<th>","状態","</th>";
+  echo "</thead></tr>";
+  echo "<tbody>";
+  foreach($result as $approvalPending) {
+    echo "<tr>";
+    echo "<td>", $approvalPending['name'], "</td>";
+    echo "<td>", $approvalPending['begin'], "</td>";
+    echo "<td>", $approvalPending['status'], "</td>";
+    echo "</tr>";
+  }
+  echo "<tbody>";
+  echo "</table>";
+  echo"<br>";
+  ?>
 
-echo "<table border='1'>";
-echo "<thead><tr>";
-echo "<th>","講師名","</th>";
-echo "<th>","日付","</th>";
-echo "</thead></tr>";
-echo "<tbody>";
-foreach($result as $approvalPending) {
-  echo "<tr>";
-  echo "<td>", $approvalPending['name'], "</td>";
-  echo "<td>", $approvalPending['begin'], "</td>";
-  echo "</tr>";
-}
-echo "<tbody>";
-echo "</table>";
-?>
-<button id="company_valution_button"><a href="company_mainpage.php">評価登録評価する</a></button>
+  <p class"company_valution">評価登録</p>
+  <?php
+  $user = "root";
+  $pass = "mariadb";
+  $dbh =new PDO('mysql:host=localhost;dbname=lcmatching_db;charset=utf8', $user, $pass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "SELECT id,name, begin, status FROM lecture,status WHERE lecture.id = lecture_id AND status = 2";
+  $stmt = $dbh->query($sql);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $dbh = null;
 
-<p class="company_search_item" >講師検索</p>
-<?php
-$user = "root";
-$pass = "mariadb";
-$dbh =new PDO('mysql:host=localhost;dbname=lcmatching_db;charset=utf8', $user, $pass);
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// 日付データ
-$sql = "SELECT id,name, begin, status FROM lecture,status WHERE lecture.id = lecture_id";
-$freeDay = "SELECT begin FROM freeday";
-$freeDaystml = $dbh->query($freeDay);
-$freeDayResult = $freeDaystml->fetchAll(PDO::FETCH_ASSOC);
-// スキルデータ
-$sql = "SELECT id,name,skill_id FROM lecture,skill_table WHERE lecture.id = lecture_id";
-$skill = "SELECT skill_id FROM skill_table";
-$skillStml = $dbh->query($skill);
-$skillResult = $skillStml->fetchAll(PDO::FETCH_ASSOC);
-$dbh = null;
-var_dump($skillResult);
-?>
+  echo "<table border='1' class='hyouka'>";
+  echo "<thead><tr>";
+  echo "<th>","講師名","</th>";
+  echo "<th>","日付","</th>";
+  echo "<th>","状態","</th>";
+  echo "<th>","id","</th>";
+  echo "<th>","送信","</th>";
+  echo "<th>","評価","</th>";
+  echo "</thead></tr>";
+  echo "<tbody>";
+  foreach($result as $approvalPending) {
+    echo "<tr>";
+    echo "<td>", $approvalPending['name'], "</td>";
+    echo "<td>", $approvalPending['begin'], "</td>";
+    echo "<td>", $approvalPending['status'], "</td>";
+    echo "<td>", $approvalPending['id'], "</td>";
+    ?>
+    <form action="company_mainpage.php" method="POST">
+      <td>
+        <select name="evaluationCount">
+          <option value="">評価を選択してください</option>
+        <option value="1000-00-00">1</option>
+        <option value="2000-00-00">2</option>
+        <option value="3000-000-00">3</option>
+        <option value="4000-00-00">4</option>
+        <option value="5000-00-00">5</option>
+      </select>
+      </td>
+      <td>
+        <input type="hidden" name="evaluationId" value="<?php echo $approvalPending['id']; ?>">
+        <input type="submit" value="評価する">
+      </td>
+    </form>
+  <?php
+    echo "</tr>";
+  }
+  echo "<tbody>";
+  echo "</table>";
+  ?>
 
-期間：
-<form method="POST" action="search_result.php">
-  <select name="begin-end">
-<?php
-    foreach ($freeDayResult as $freeDay) { ?>
-      <option value="<?php echo $freeDay['begin']; ?>"><?php echo $freeDay['begin']; ?></option>
-<?php
+  <p class="company_search_item" >講師検索</p>
+  <?php
+  $user = "root";
+  $pass = "mariadb";
+  $dbh =new PDO('mysql:host=localhost;dbname=lcmatching_db;charset=utf8', $user, $pass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // 日付データ
+  $sql = "SELECT id,name, begin, status FROM lecture,status WHERE lecture.id = lecturer_id";
+  $freeDay = "SELECT begin FROM freeday";
+  $freeDaystml = $dbh->query($freeDay);
+  $freeDayResult = $freeDaystml->fetchAll(PDO::FETCH_ASSOC);
+  // スキルデータ
+  $sql = "SELECT id,name,skill_id FROM lecture,skill_table WHERE lecture.id = lecture_id";
+  $skill = "SELECT * FROM skill_table inner join skill_master on skill_table.skill_id = skill_master.id";
+  $skillStml = $dbh->query($skill);
+  $skillResult = $skillStml->fetchAll(PDO::FETCH_ASSOC);
+  $dbh = null;
+  ?>
+
+  期間：
+  <form method="POST" action="search_result.php">
+    <input type="date" type="date" name="date">
+    <select name="skill">
+  <?php
+      foreach ($skillResult as $skill) { ?>
+        <option value="<?php echo $skill['skill_id']; ?>"><?php echo $skill['skilltype']; ?></option>
+  <?php
+      }
+  ?>
+    </select>
+    <input type="submit" value="送信">
+    </form>
+
+  <?php
+    if (isset($_POST['evaluationId'])) {
+      $evalutionData = $_POST['evaluationCount'];
+      $evalutionId = $_POST['evaluationId'];
+
+      $user = "root";
+      $pass = "mariadb";
+      $dbh =new PDO('mysql:host=localhost;dbname=lcmatching_db;charset=utf8', $user, $pass);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "UPDATE status SET evaluation = '$evalutionData' where lecture_id = '$evalutionId'";
+      $stmt = $dbh->query($sql);
+      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $dbh = null;
     }
-?>
-  </select>
-  <select name="ski">
-<?php
-    foreach ($skillResult as $skill) { ?>
-      <option value="<?php echo $skill['skill_id']; ?>"><?php echo $skill['skill_id']; ?></option>
-<?php
-    }
-?>
-  </select>
-  <input type="submit" value="送信">
-  </form>
+    ?>
+
 </body>
+</style>
 </html>
--- search_result.php
+++ search_result.php
@@ -6,75 +6,72 @@
 </head>
   <body>
     <div>
-
   <?php
+      $date = $_POST['date'];
+      $mozi = mb_substr($date, 0, 7);
+      $firstDate = $mozi.'-00';
+      $lastDate = $mozi.'-31';
+      $wantSkill = $_POST['skill'];
       $user = 'root';
       $password = 'mariadb';
       $dbName = 'lcmatching_db';
       $host = 'localhost:3306';
       $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
-      //データベースに接続
       try{
       $pdo = new PDO($dsn, $user, $password);
       $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       echo "データベース{$dbName}に接続しました。","<br>";
 
-      //$sql = "SELECT * FROM lecture";
-      $sql = "SELECT * FROM lecture JOIN freeday ON lecture.id = freeday.lecturer_id JOIN skill_table ON lecture.id = skill_table.lecturer_id ";
-      $stm = $pdo->prepare($sql);
-      $stm->execute();
+      $sql = "SELECT * FROM lecture JOIN freeday ON lecture.id = freeday.lecturer_id JOIN skill_table ON lecture.id = skill_table.lecturer_id WHERE freeday.begin BETWEEN '$firstDate' AND '$lastDate'  AND skill_table.skill_id = '$wantSkill'";
+      $stm = $pdo->query($sql);
       $results = $stm->fetchAll(PDO::FETCH_ASSOC);
       echo "<pre>";
       var_dump($results);
       echo "<pre>";
       ?>
 
-      <form method="POST" action="confirm_offer.php">
-        <ul>
+        <form method="POST" action="confirm_offer.php">
+          <?php
+          //テーブルのタイトル行
+          echo "<table class>";
+          echo "<thead><tr>";
+          echo "<th>","","</th>";
+          echo "<th>","ID","</th>";
+          echo "<th>","名前","</th>";
+          echo "<th>","電話番号","</th>";
+          echo "<th>","アドレス","</th>";
+          echo "<th>","スキル","</th>";
+          echo "<th>","日付","</th>";
+          echo "</tr></thead>";
 
-      <?php
-      //テーブルのタイトル行
-      echo "<table>";
-      echo "<thead><tr>";
-      echo "<th>","","</th>";
-      echo "<th>","ID","</th>";
-      echo "<th>","名前","</th>";
-      echo "<th>","電話番号","</th>";
-      echo "<th>","アドレス","</th>";
-      echo "<th>","スキル","</th>";
-      echo "<th>","日付","</th>";
-      echo "</tr></thead>";
-
-      foreach($results as $row) {
-        echo "<tr>";
-        echo "<td>",'<input type="checkbox" name="check[]" value=',$row['id'],'>',"</td>";
-        echo "<td>",$row['id'],"</td>";
-        echo "<td>",$row['name'],"</td>";
-        echo "<td>",$row['tel'],"</td>";
-        echo "<td>",$row['mail_address'],"</td>";
-        echo "<td>",$row['skill_id'],"</td>";
-        echo "<td>",$row['begin'],"</td>";
-        $array = array($row['id']);
-        $ID = implode(",", $array);
-        echo $ID;
-        echo "</tr>";
-      }
-      echo "<tbody>";
-      echo "</table>";
+        foreach($results as $row) {
+          echo "<tr>";
+          echo "<td>",'<input type="checkbox" name="check[]" value=',$row['id'],'>',"</td>";
+          echo "<td>",$row['id'],"</td>";
+          echo "<td>",$row['name'],"</td>";
+          echo "<td>",$row['tel'],"</td>";
+          echo "<td>",$row['mail_address'],"</td>";
+          echo "<td>",$row['skill_id'],"</td>";
+          echo "<td>",$row['begin'],"</td>";
+          $array = array($row['id']);
+          $ID = implode(",", $array);
+          echo $ID;
+          echo "</tr>";
+        }
+        echo "<tbody>";
+        echo "</table>";
 
       } catch (Exception $e){
-        echo '<span class="error">エラーがありました。</span><br>';
-        echo $e->getMessage();
-        exit();
+          echo '<span class="error">エラーがありました。</span><br>';
+          echo $e->getMessage();
+          exit();
       }
-
-
       ?>
-      <input type="button" value="戻る" onclick="location.href='company_mainpage.php'">
-      <input type="submit" value="決定">
-    </ul>
-  </form>
+        <input type="button" value="戻る" onclick="location.href='company_mainpage.php'">
+        <input type="submit" value="決定">
+      </ul>
+      </form>
     </div>
   </body>
 </html>
